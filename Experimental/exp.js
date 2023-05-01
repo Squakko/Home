4c4718b3d287ff5ae9329ba840d84ef37ba5dcb1
minhlienb <131936442+minhlienb@users.noreply.github.com>
2023-05-01 09:58:14 +0700

    Add files via upload

---
+++ Experimental/exp.js
@@ -0,0 +1,11 @@
+function ShowScriptText() {
+    //document.getElementById("lead-sentence").innerHTML = "";
+    // document.getElementsByClassName("lead-sentence")[0].innerHTML = "Bat ngo라인에";
+    // document.getElementsByClassName("lead-sentence")[0].innerHTML = "fdsf";
+    // document.getElementsByClassName("lead-sentence")[0].innerHTML = "bla bla";
+    // document.getElementsByClassName("lead-sentence")[0].innerHTML = "bla bla 2";
+}
+
+function HoverArrow() {
+    document.getElementById("arrow-go-down").play();
+}
